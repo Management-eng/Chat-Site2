7b2929b57447cbe8a18fb55411cbad012ad5db83
Management-cpu <Rebeccaeffiong5451@gmail.com>
2025-04-13 14:33:38 +0100

    Update home.js

--- public/home.js
+++ public/home.js
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <div class="placeholder-content">
             <i class="fas fa-home fa-3x"></i>
             <h3>Home Dashboard</h3>
-            <p>This is your personal dashboard. View your account information and settings here.</p>
+            <p>It our priority to keep and value our customer policies.</p>
           </div>
         `
         break
